2daaf828873d24f423107ce3f0b0107350456f59
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-27 08:40:37 -0400

    Add files via upload

---
+++ 0017/main.js
@@ -0,0 +1,28 @@
+//This program reads from an existing text file and outputs the content
+//of the text file to the webpage.
+
+//How to run the program:
+//open the terminal
+//type: node main.js
+//open a web browser
+//type: http://localhost:1234
+
+var fs_obj = require('fs');
+var http_obj = require('http');
+
+var read_file_address = __dirname + "/" + "data.txt"; 
+var read_stream_obj = fs_obj.createReadStream(read_file_address);
+
+function server_function(request, response)
+{
+	response.writeHead(200, {'Content-Type': 'text/html'});  
+    //we specify 'html' so that it gets treated as an HTML file (instead of plain text file)
+    
+    read_stream_obj.pipe(response);  
+    //pipe the "read_stream_obj" to the "response"
+}
+
+var server_obj = http_obj.createServer(server_function);
+
+server_obj.listen(1234);
+console.log("listening to the port 1234");
